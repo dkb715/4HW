e7ec3c332d1666b5137e0db3ed0f3a6fc9d63c4d
Donavan Brown <dkb@Donavans-MacBook-Pro.local>
2019-12-10 18:32:01 -0500

    overall update

---
+++ questions.js
@@ -0,0 +1,26 @@
+var quizStarter = document.querySelector("#start");
+var container = document.querySelector(".container");
+var textArea = document.querySelector("#textarea");
+var question1 = true;
+var question1 = false;
+var question2 = true;
+var question2 = false;
+var question3 = true;
+var question3 = false;
+var questionBox = "";
+var finalArray = [];
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    }]
+quizStarter.addEventListener("click", function() {
+  if (quizStarter) {
+      (questions.textcontent) 
+  }
+});
